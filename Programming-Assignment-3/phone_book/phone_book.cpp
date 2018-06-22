61a462c75cbda650e5f5d1235c1c1669f7829976
Valentin Popov <valeo.popov@gmail.com>
2018-06-22 16:25:06 +0300

    phone book is implemented

--- Programming-Assignment-3/phone_book/phone_book.cpp
+++ Programming-Assignment-3/phone_book/phone_book.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <map>
 
 using std::string;
 using std::vector;
 using std::cin;
+using std::map;
 
 struct Query {
     string type, name;
@@ -32,35 +34,19 @@ void write_responses(const vector<string>& result) {
 
 vector<string> process_queries(const vector<Query>& queries) {
     vector<string> result;
-    // Keep list of all existing (i.e. not deleted yet) contacts.
-    vector<Query> contacts;
+    // user map between number and name for best performance
+    map<int,string> contacts;
     for (size_t i = 0; i < queries.size(); ++i)
         if (queries[i].type == "add") {
-            bool was_founded = false;
-            // if we already have contact with such number,
-            // we should rewrite contact's name
-            for (size_t j = 0; j < contacts.size(); ++j)
-                if (contacts[j].number == queries[i].number) {
-                    contacts[j].name = queries[i].name;
-                    was_founded = true;
-                    break;
-                }
-            // otherwise, just add it
-            if (!was_founded)
-                contacts.push_back(queries[i]);
+            contacts[queries[i].number] = queries[i].name;
         } else if (queries[i].type == "del") {
-            for (size_t j = 0; j < contacts.size(); ++j)
-                if (contacts[j].number == queries[i].number) {
-                    contacts.erase(contacts.begin() + j);
-                    break;
-                }
+            contacts.erase(queries[i].number);
         } else {
             string response = "not found";
-            for (size_t j = 0; j < contacts.size(); ++j)
-                if (contacts[j].number == queries[i].number) {
-                    response = contacts[j].name;
-                    break;
-                }
+            auto it = contacts.find(queries[i].number);
+            if (it != contacts.end()) {
+                response = it->second;
+            }
             result.push_back(response);
         }
     return result;
